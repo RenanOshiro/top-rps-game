78960206fc5f6916005028f4eceeae3f7bf1a313
Renan Kenji Oshiro <103371753+RenanOshiro@users.noreply.github.com>
2025-07-05 00:17:19 -0300

    Edit script.js, add functions playRound, playGame and showResults

--- script.js
+++ script.js
@@ -1,28 +1,91 @@
-let humanScore = 0, cpuScore = 0;
+let humanScore = 0, cpuScore = 0, draw = 0;
 
 function getComputerChoice() {
-    let cpuOption = 0;
+    let cpuChoice = 0;
     while (true) {
-        cpuOption = Math.floor(Math.random() * 10)
-        if (cpuOption == 1) {
+        cpuChoice = Math.floor(Math.random() * 10)
+        if (cpuChoice == 1) {
             return "Rock";
-        } else if (cpuOption == 2) {
+        } else if (cpuChoice == 2) {
             return "Paper";
-        } else if (cpuOption == 3) {
+        } else if (cpuChoice == 3) {
             return "Scissors";
         }
     }
 }
 
 function getHumanChoice() {
-    let humOption = prompt("Choose your play: ");
-    if (humOption == 1) {
+    let humChoice = prompt("1 - Rock\n2 - Paper\n3 - Scissors\nChoose your play:");
+    if (humChoice == 1) {
         return "Rock";
-    } else if (humOption == 2) {
+    } else if (humChoice == 2) {
         return "Paper";
-    } else if (humOption == 3) {
+    } else if (humChoice == 3) {
         return "Scissors";
     }
 }
 
-console.log(getHumanChoice());
+function playRound(humChoice, cpuChoice) {
+    switch(humChoice) {
+        case "Rock":
+            if (cpuChoice == "Rock") {
+                draw++;
+                console.log("Draw! Rock x Rock.")
+                break;
+            } else if (cpuChoice == "Paper") {
+                cpuScore++;
+                console.log("You Lost! Paper beats Rock!")
+                break;
+            } else {
+                humanScore++;
+                console.log("You Won! Rock beats Scissors!")
+                break;
+            }
+        case "Paper":
+            if (cpuChoice == "Rock") {
+                humanScore++;
+                console.log("You Won! Paper beats Rock!")
+                break;
+            } else if (cpuChoice == "Paper") {
+                draw++;
+                console.log("Draw! Paper x Paper.")
+                break;
+            } else {
+                cpuScore++;
+                console.log("You Lost! Scissors beats Paper!")
+                break;
+            }
+        case "Scissors":
+            if (cpuChoice == "Rock") {
+                cpuScore++;
+                console.log("You Lost! Rock beats Scissors!")
+                break;
+            } else if (cpuChoice == "Paper") {
+                humanScore++;
+                console.log("You Won! Scissors beats Paper!")
+                break;
+            } else {
+                draw++;
+                console.log("Draw! Scissors x Scissors.")
+                break;
+            }
+    }
+    return;
+}
+
+function showResults() {
+    console.log(`You won ${humanScore} round(s)!`)
+    console.log(`You lost ${cpuScore} round(s)!`)
+    console.log(`Draw ${draw} round(s)!`)
+}
+
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        const hum = getHumanChoice();
+        const cpu = getComputerChoice();
+        playRound(hum, cpu);
+    }
+    showResults();
+}
+
+playGame();
